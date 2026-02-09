ff10e42fc20c48546777e7e40562d6c1694c9bf9
Muhammad Fauzan <fauzandewa783@gmail.com>
2026-02-09 23:59:41 +0800

    fix room join bug

--- server.js
+++ server.js
@@ -9,37 +9,36 @@ const rooms = {}; // Simpan room & password
 
 io.on("connection", (socket) => {
 
-  socket.on("joinRoom", ({ roomName, password, username }) => {
+ socket.on("joinRoom", ({ roomName, password, username }) => {
 
-    // Kalau room belum ada → buat baru
-    if (!rooms[roomName]) {
-      rooms[roomName] = password;
-    }
+  if (!roomName || !password) {
+    socket.emit("errorMessage", "Room dan password wajib diisi!");
+    return;
+  }
 
-    // Cek password
-    if (rooms[roomName] !== password) {
-      socket.emit("errorMessage", "Password salah!");
-      return;
-    }
+  // Kalau room belum ada → buat
+  if (!rooms[roomName]) {
+    rooms[roomName] = password;
+  }
 
-    socket.join(roomName);
-    socket.room = roomName;
-    socket.username = username || "Anonymous";
+  // Kalau password salah
+  if (rooms[roomName] !== password) {
+    socket.emit("errorMessage", "Password salah!");
+    return;
+  }
 
-    io.to(roomName).emit("message", {
-      user: "SYSTEM",
-      text: socket.username + " joined the room"
-    });
+  socket.join(roomName);
+  socket.room = roomName;
+  socket.username = username || "Anonymous";
+
+  socket.emit("joinedSuccess"); // kirim konfirmasi sukses
+
+  io.to(roomName).emit("message", {
+    user: "SYSTEM",
+    text: socket.username + " joined the room"
   });
+});
 
-  socket.on("chatMessage", (msg) => {
-    if (!socket.room) return;
-
-    io.to(socket.room).emit("message", {
-      user: socket.username,
-      text: msg
-    });
-  });
 
   socket.on("disconnect", () => {
     if (socket.room) {
